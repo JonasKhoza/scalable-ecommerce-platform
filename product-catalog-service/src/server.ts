bcdd130eff6635509ad7682fc7dc345f75711526
Jonas Khoza <jonaskhoza18@gmail.com>
2025-01-03 17:06:52 +0200

    Setup database config"

--- product-catalog-service/src/server.ts
+++ product-catalog-service/src/server.ts
@@ -1,7 +1,10 @@
 import app from "./app";
+import { onShutDowngracefullyClosePool } from "./utils/databaseConfig";
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}!`);
 });
+
+onShutDowngracefullyClosePool();
